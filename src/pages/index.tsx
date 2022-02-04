98eff9b27a3307cf43b7a288ea43563cac93a22c
Sai Krishna <skris4291@gmail.com>
2022-02-05 00:56:50 +0530

    feat!: remove js in /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { Box } from "@chakra-ui/react"
 import Hero from "../components/Hero"
 
+export const config = {
+  unstable_runtimeJS: false
+};
 
 const Index = () => (
   <Box w="100vw" p={5}>
