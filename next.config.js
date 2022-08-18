0922e7735d2faa0f0e5214c626012d115de29f64
Sai Krishna <3975305+s-kris@users.noreply.github.com>
2022-08-18 19:40:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,35 +1,36 @@
  const nextConfig = {
+         
           async rewrites() {
               return {
                   beforeFiles: [
       
                       {
                           "source": "/blog",
-                          "destination": "https://blogcontent.superblog.cloud/"
+                          "destination": "https://qwikskills.superblog.cloud/"
                       },
                       {
-                          "source": "/blog/(.*?)([.]\w{2,5})(\?[^/]+)?",
-                          "destination": "https://blogcontent.superblog.cloud/$1$2$3"
+                          "source": "/blog/(.*?)([.]\\w{2,5})(\\?[^/]+)?",
+                          "destination": "https://qwikskills.superblog.cloud/$1$2$3"
                       },
                       {
-                          "source": "/blog/([\S\s]+)",
-                          "destination": "https://blogcontent.superblog.cloud/$1/"
+                          "source": "/blog/([\\S\\s]+)",
+                          "destination": "https://qwikskills.superblog.cloud/$1/"
                       },
                       {
-                          "source": "/_elderjs/svelte/(.*?)([.]\w{2,5})(\?[^/]+)?",
-                          "destination": "https://blogcontent.superblog.cloud/_elderjs/svelte/$1$2$3"
+                          "source": "/_elderjs/svelte/(.*?)([.]\\w{2,5})(\\?[^/]+)?",
+                          "destination": "https://qwikskills.superblog.cloud/_elderjs/svelte/$1$2$3"
                       },
                       {
-                          "source": "/_elderjs/svelte/([\S\s]+)",
-                          "destination": "https://blogcontent.superblog.cloud/_elderjs/svelte/$1"
+                          "source": "/_elderjs/svelte/([\\S\\s]+)",
+                          "destination": "https://qwikskills.superblog.cloud/_elderjs/svelte/$1"
                       },
                       {
-                          "source": "/.netlify/functions/(.*?)([.]\w{2,5})(\?[^/]+)?",
-                          "destination": "https://blogcontent.superblog.cloud/.netlify/functions/$1$2$3"
+                          "source": "/.netlify/functions/(.*?)([.]\\w{2,5})(\\?[^/]+)?",
+                          "destination": "https://qwikskills.superblog.cloud/.netlify/functions/$1$2$3"
                       },
                       {
-                          "source": "/.netlify/functions/([\S\s]+)",
-                          "destination": "https://blogcontent.superblog.cloud/.netlify/functions/$1"
+                          "source": "/.netlify/functions/([\\S\\s]+)",
+                          "destination": "https://qwikskills.superblog.cloud/.netlify/functions/$1"
                       },
                   ]
               }
